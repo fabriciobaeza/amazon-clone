c46808e0879c4f735125d655956e01d6bde919c5
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-02-09 22:32:28 -0500

    Reducer

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const addToBasket = () => {
+    // dispatch the item to the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -18,11 +33,8 @@ function Product({ title, image, price, rating }) {
             ))}
         </div>
       </div>
-      <img
-        src={image}
-        alt=""
-      />
-      <button>Add to Basket</button>
+      <img src={image} alt="" />
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
