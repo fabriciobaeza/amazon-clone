a9b439837b8918875139aef5ed31713b2c921df9
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-02-13 21:32:57 -0500

    Login Page

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 
 function Header() {
-  const [{basket}, state, dispatch] = useStateValue();
+  const [{ basket }, state, dispatch] = useStateValue();
 
   return (
     <div className="header">
@@ -22,15 +22,20 @@ function Header() {
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
       </div>
+
       <div className="header__nav">
+        <Link to="/login">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
         <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Hello</span>
-        </div>
-        <div className="header__option">
-          <span className="header__optionLineOne">Sign In</span>
+          <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
         </div>
+
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
@@ -38,7 +43,9 @@ function Header() {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
---
+++ src/Login.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          className="login__logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign In</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="login__signInButton"
+            onClick={signIn}
+            type="submit"
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to AMAZON CLONE Conditions of Use & Sale.
+          Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="login__registerButton" onClick={register}>
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
