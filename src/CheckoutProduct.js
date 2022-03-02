189dcf2d7069ec08228840cb8438788b7ebb69aa
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-03-01 22:46:34 -0500

    Orders Page

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({ id, image, title, price, rating }) {
-  const [{basket}, dispatch] = useStateValue();
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+  const [{ basket }, dispatch] = useStateValue();
   const removeFromBasket = () => {
     dispatch({
-      type: 'REMOVE_FROM_BASKET',
-      id: id, 
-    })
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
   };
   return (
     <div className="checkoutProduct">
-      <img src={image} className="checkoutProduct__image" alt="product"/>
+      <img src={image} className="checkoutProduct__image" alt="product" />
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
@@ -26,7 +26,9 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>⭐</p>
             ))}
         </div>
-        <button onClick={removeFromBasket}>Remove from basket</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from basket</button>
+        )}
       </div>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -8,12 +8,12 @@ import { auth } from "./firebase";
 
 function Header() {
   const [{ basket, user }] = useStateValue();
-  
+
   const handleAuthentication = () => {
     if (user) {
       auth.signOut();
     }
-  }
+  };
 
   return (
     <div className="header">
@@ -31,19 +31,23 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to={!user && '/login'}>
+        <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header__option">
-            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+            <span className="header__optionLineOne">
+              Hello {!user ? "Guest" : user.email}
+            </span>
             <span className="header__optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
             </span>
           </div>
         </Link>
 
-        <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">& Orders</span>
-        </div>
+        <Link to="/orders">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
 
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
