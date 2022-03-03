4c427cf6e368688fed74922efeec09c8e334c070
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-03-02 23:31:42 -0500

    Deploy

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5002/clone-bb20e/us-central1/api",
+  baseURL: "https://us-central1-clone-bb20e.cloudfunctions.net/api",
+  // "http://localhost:5002/clone-bb20e/us-central1/api"
 });
 
 export default instance;
+
